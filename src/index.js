fed64105208bef1aaba4a34eed6c523ed2991549
Ansh2905 <anshdpatel10@gmail.com>
2022-07-09 16:34:44 +0530

    Add error message for incorrect input

--- src/index.js
+++ src/index.js
@@ -6,16 +6,22 @@ search_button.addEventListener("click", function(){
 async function getNewInputData(new_input){
     let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${new_input}&APPID=c29b4a211db56e2c54591d2330d2d5cd`);
     let required_data = await response.json();
-    console.log(required_data);
-    changeData(required_data.name, "city-name", "", "");
-    changeWeatherPic(required_data.weather[0].icon);
-    console.log(required_data.weather[0].icon);
-    changeData(Math.round(required_data.main.temp - 273), "main-temperature", "", "&#8451;");
-    changeData(Math.round(required_data.main.feels_like - 273), "main-feels-like", "Feels like: ", "&#8451;");
-    changeData(required_data.main.humidity, "main-humidity", "Humidity: ", "%");
-    changeData(required_data.weather[0].description[0].toUpperCase() + required_data.weather[0].description.substring(1), "main-clouds", "", "");
-    changeData(Math.round(required_data.main.temp_max - 273), "max-temp", "", "&#8451;");
-    changeData(Math.round(required_data.main.temp_min - 273), "min-temp", "", "&#8451;");
+    if(required_data.cod != "200"){
+        document.getElementById('error-input').style.display = "block";
+    }
+    else{
+        document.getElementById('error-input').style.display = "none";
+        console.log(required_data);
+        changeData(required_data.name, "city-name", "", "");
+        changeWeatherPic(required_data.weather[0].icon);
+        console.log(required_data.weather[0].icon);
+        changeData(Math.round(required_data.main.temp - 273), "main-temperature", "", "&#8451;");
+        changeData(Math.round(required_data.main.feels_like - 273), "main-feels-like", "Feels like: ", "&#8451;");
+        changeData(required_data.main.humidity, "main-humidity", "Humidity: ", "%");
+        changeData(required_data.weather[0].description[0].toUpperCase() + required_data.weather[0].description.substring(1), "main-clouds", "", "");
+        changeData(Math.round(required_data.main.temp_max - 273), "max-temp", "", "&#8451;");
+        changeData(Math.round(required_data.main.temp_min - 273), "min-temp", "", "&#8451;");
+    }
 }
 
 function changeData(specific_data, specific_id, pre, post){
